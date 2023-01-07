974a150dc7e5aaec4d838560ad4087fdba9fb89a
maycon araujo <maycon.araujo.santos@gmail.com>
2023-01-06 22:14:45 -0300

    chore: first commit

---
+++ src/main/java/com/airlines/booking/onetwothreemiles/OnetwothreemilesApplication.java
@@ -0,0 +1,13 @@
+package com.airlines.booking.onetwothreemiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnetwothreemilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnetwothreemilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airlines/booking/onetwothreemiles/OnetwothreemilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airlines.booking.onetwothreemiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnetwothreemilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
